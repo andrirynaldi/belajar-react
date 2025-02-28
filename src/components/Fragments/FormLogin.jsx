05151e8c84830d0378021721edbbf806decdbde5
unknown <andrirynaldi@ymail.com>
2025-02-28 15:35:19 +0800

    belajar useRef

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react"
 import Button from "../elements/Button"
 import InputForm from "../elements/Input"
 const FormLogin = (props) => {
@@ -9,9 +10,14 @@ const FormLogin = (props) => {
         console.log('login success');
         window.location.href = '/products'
     }
+    const emailRef = useRef(null)
+
+    useEffect(() => {
+        emailRef.current.focus()
+    }, [])
     return (
         <form onSubmit={handleLogin}>
-            <InputForm placeholder='example@mail.com' type='email' name='email' label='Email' htmlFor='email' />
+            <InputForm placeholder='example@mail.com' type='email' name='email' label='Email' htmlFor='email' ref={emailRef} />
             <InputForm placeholder='....' type='password' name='password' label='Password' htmlFor='password' />
             <Button variant="bg-blue-600 w-full" type='submit'>Login</Button>
         </form>
--- src/components/elements/Input/index.jsx
+++ src/components/elements/Input/index.jsx
@@ -1,13 +1,16 @@
 import Label from './Label'
 import Input from './Input'
+import { forwardRef } from 'react'
 
-const InputForm = (props) => {
-    const { htmlFor, label, type, name, placeholder } = props
-    return (
-        <div className="mb-6">
-            <Label htmlFor={htmlFor}>{label}</Label>
-            <Input type={type} placeholder={placeholder} name={name} />
-        </div>
-    )
-}
+const InputForm = forwardRef(
+    (props, ref) => {
+        const { htmlFor, label, type, name, placeholder } = props
+        return (
+            <div className="mb-6">
+                <Label htmlFor={htmlFor}>{label}</Label>
+                <Input type={type} placeholder={placeholder} name={name} ref={ref} />
+            </div>
+        )
+    }
+)
 export default InputForm
